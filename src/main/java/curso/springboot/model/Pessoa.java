e08abd7ac12e9a63ea6b1318233bdf4c97bd650e
Wesley Marques Saraiva <wesleysaraiva109@gmail.com>
2021-12-20 15:08:00 -0300

    commit app

---
+++ src/main/java/curso/springboot/model/Pessoa.java
@@ -0,0 +1,65 @@
+package curso.springboot.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+public class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String nome;
+	
+	@CPF
+	@Column(unique = true)
+	private String cpf;
+	
+	@Column(unique = true)
+	private String cafemanha;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCafemanha() {
+		return cafemanha;
+	}
+
+	public void setCafemanha(String cafemanha) {
+		this.cafemanha = cafemanha;
+	}
+
+}
